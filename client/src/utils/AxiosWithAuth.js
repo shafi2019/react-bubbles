ff1ff7d6cae2520b3f1a39cd241cd1925a206805
Shafi Masoumi <masoumi2017@icloud.com>
2019-12-14 09:25:50 -0800

    AxiosWithAuth Done

---
+++ client/src/utils/AxiosWithAuth.js
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return Axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+      Authorization: token
+    }
+  });
+};
